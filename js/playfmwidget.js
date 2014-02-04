13e0ce85ad77678ca59cfd9f261237503c3aecb9
Federico Giust <fgiustdj@gmail.com>
2014-02-04 18:39:12 -0200

    Update
    Updates style
    Update example
    New options
    Use YQL to get cross domain data

--- js/playfmwidget.js
+++ js/playfmwidget.js
@@ -24,21 +24,21 @@
                 element = $(this).attr('id');
 				//console.log('http://api.play.fm/artist/search/' + apiKey + '/name/' + settings.artistName.replace(' ', '+'));	
 				$.support.cors = true;
-				
 				jQuery.ajax({
 
-				  type: 'GET',
-				  url: 'http://api.play.fm/artist/search/' + apiKey + '/name/' + settings.artistName.replace(' ', '+'),
-				  dataType: 'xml',
-				  success: buildWidget
+				  	type: 'GET',
+			      	crossDomain: true,
+				  	url: 'http://query.yahooapis.com/v1/public/yql?q=' + encodeURIComponent('select * from xml where url="http://api.play.fm/artist/search/' + apiKey + '/name/' + settings.artistName.replace(' ', '+') + '"') + '&format=xml&callback=',
+				  	dataType: 'xml',
+				  	success: buildWidget
 
 				});//end getJSON function
 
 
 				function buildWidget(xml){
-					var widgetTemplate ='<!---------- PLAY.FM WIDGET START ----------><div style="margin-left:auto; margin-right:auto;min-width:220px;"> <object width="100%" height="150px"><param name="allowscriptaccess" value="always" wmode="transparent"></param><param name="FlashVars" VALUE="url=http%3A%2F%2Fwww.play.fm%2Frecordings%2Fflash%2F01%2Fartist%2F[artistId]%2ForderBy%2F[orderBy]&type=large&showpic=' + settings.showPic + '&widgetGenerator=http://www.play.fm/widgets&op=true"></param><embed src="http://www.play.fm/flash/playfmWidget.swf?url=http%3A%2F%2Fwww.play.fm%2Frecordings%2Fflash%2F01%2Fartist%2F[artistId]%2ForderBy%2F[orderBy]&type=large&showpic=' + settings.showPic + '&widgetGenerator=http://www.play.fm/widgets&op=true" flashVars="url=http%3A%2F%2Fwww.play.fm%2Frecordings%2Fflash%2F01%2Fartist%2F[artistId]%2ForderBy%2F[orderBy]&type=large&showpic=' + settings.showPic + '&widgetGenerator=http://www.play.fm/widgets&op=true" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="false" wmode="transparent" width="100%" height="150px"></embed></object></div><!---------- PLAY.FM WIDGET END ---------->';
+					var widgetTemplate ='<!---------- PLAY.FM WIDGET START ----------><div style="margin-left:auto; margin-right:auto;min-width:220px;"> <object width="100%" height="150px"><param name="allowscriptaccess" value="always" wmode="transparent"></param><param name="FlashVars" VALUE="url=http%3A%2F%2Fwww.play.fm%2Frecordings%2Fflash%2F01%2Fartist%2F[artistId]%2ForderBy%2F' + settings.orderBy + '&type=large&showpic=' + settings.showPic + '&widgetGenerator=http://www.play.fm/widgets&op=true"></param><embed src="http://www.play.fm/flash/playfmWidget.swf?url=http%3A%2F%2Fwww.play.fm%2Frecordings%2Fflash%2F01%2Fartist%2F[artistId]%2ForderBy%2F' + settings.orderBy + '&type=large&showpic=' + settings.showPic + '&widgetGenerator=http://www.play.fm/widgets&op=true" flashVars="url=http%3A%2F%2Fwww.play.fm%2Frecordings%2Fflash%2F01%2Fartist%2F[artistId]%2ForderBy%2F' + settings.orderBy + '&type=large&showpic=' + settings.showPic + '&widgetGenerator=http://www.play.fm/widgets&op=true" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="false" wmode="transparent" width="100%" height="150px"></embed></object></div><!---------- PLAY.FM WIDGET END ---------->';
 				   	var artistId = $(xml).find("artist").attr("id");
-
+				   	console.log('xml ' + xml);
 				   	widgetTemplate = widgetTemplate.replace('[artistId]', artistId).replace('[artistId]', artistId).replace('[artistId]', artistId);
 
 
